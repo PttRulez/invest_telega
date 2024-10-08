088aace358274703556945eeec2cab475b8a48f0
satoshi <sashafem@mail.ru>
2024-10-08 10:12:10 +0300

    first

---
+++ go.mod
@@ -0,0 +1,8 @@
+module invest_telega
+
+go 1.22.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/telebot.v3 v3.3.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	botToken := os.Getenv("TOKEN")
+
+	pref := tele.Settings{
+		Token:  botToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/getid", func(c tele.Context) error {
+		return c.Send(fmt.Sprintf("Ваш ID: %d", c.Chat().ID))
+	})
+
+	b.Start()
+}
